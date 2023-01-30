0b4e16f0100f05c53d941ae320a28001561d06d2
Lauris <lauriselvijsm@gmail.com>
2023-01-30 16:15:33 +0200

    added dark theme

--- src/components/TodoItemCount/TodoInputCount.component.tsx
+++ src/components/TodoItemCount/TodoInputCount.component.tsx
@@ -4,16 +4,10 @@ import pluralize from "pluralize";
 import { RootState } from "../../store/app/store";
 import { todoSliceName } from "../../store/features/Todo";
 
-import styles from "./TodoItemCount.style.module.scss";
-
 const TodoInputCount = () => {
   const { tasksLeft } = useSelector((state: RootState) => state[todoSliceName]);
 
-  return (
-    <div className={styles.todoInputCount}>
-      {pluralize("item", tasksLeft, true)} left
-    </div>
-  );
+  return <div>{pluralize("item", tasksLeft, true)} left</div>;
 };
 
 export default TodoInputCount;
