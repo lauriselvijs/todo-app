4933344fef70aed6fb22cc84b9fbb06d343022bc
Lauris Mačāns <85683069+lauriselvijs@users.noreply.github.com>
2023-03-06 10:15:44 +0000

    minor fix

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -1,20 +1,27 @@
 import { createNewTodoPlaceholder } from "../constants/Todo.const";
 
 describe("Todo", () => {
-  it("should be able to create task", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+
+  it("should be able to create task", () => {
     cy.findByPlaceholderText(createNewTodoPlaceholder).type("Finnish project");
-    cy.findByRole("button", { name: /check if task completed/i }).click();
     cy.findByPlaceholderText(createNewTodoPlaceholder).type("{enter}");
+
     cy.findByPlaceholderText(createNewTodoPlaceholder).should("have.value", "");
     cy.findByText("Finnish project").should("exist");
+
     cy.findByText("Finnish project")
       .findByRole("button", { name: /check if task completed/i })
       .click();
-    // cy.get(".todo-list-item-completed").should(
-    //   "have.css",
-    //   "background-repeat",
-    //   "no-repeat, no-repeat"
-    // );
+
+    cy.findByText("Finnish project")
+      .findByRole("button", { name: /check if task completed/i }).should(
+      "have.css",
+      "background-repeat",
+      "no-repeat, no-repeat"
+    );
   });
 });
